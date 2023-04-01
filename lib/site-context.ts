c7c04b5b2f273708ab84272aa300ef9e629f2f75
YTG2G3 <scratchlover05@gmail.com>
2023-04-01 01:01:20 -0700

    api/user

---
+++ lib/site-context.ts
@@ -0,0 +1,6 @@
+// Provide global context
+import { createContext } from "react";
+
+let SiteContext = createContext({});
+
+export default SiteContext;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,48 @@ import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import theme from '@/lib/theme';
 import Head from 'next/head';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import SiteContext from '@/lib/site-context';
 
 export default function App({ Component, pageProps }: AppProps) {
+    let [user, setUser] = useState(undefined);
+    let { data: session, status } = useSession();
+
+    useEffect(() => { loadData() }, [status]);
+
+    // Load user data
+    const loadData = async () => {
+        // When loading
+        if (status === "loading") return;
+
+        // When not logged in
+        if (status === "unauthenticated") {
+            setUser(undefined);
+            localStorage.removeItem("school");
+            return;
+        }
+
+        // When logged in
+        let u = axios.get
+    }
+
     return (
         <>
             <Head>
                 <title>MyExtraDuty</title>
             </Head>
 
-            <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={theme()}
-            >
-                <Component {...pageProps} />
-            </MantineProvider>
+            <SiteContext.Provider value={{ user }}>
+                <MantineProvider
+                    withGlobalStyles
+                    withNormalizeCSS
+                    theme={theme()}
+                >
+                    <Component {...pageProps} />
+                </MantineProvider>
+            </SiteContext.Provider>
         </>
     );
 }
