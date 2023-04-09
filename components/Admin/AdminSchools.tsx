2c57729e02aba69fc6fc861b86415d0ffef50699
YTG2G3 <scratchlover05@gmail.com>
2023-04-08 19:14:57 -0700

    latest

--- components/Admin/AdminSchools.tsx
+++ components/Admin/AdminSchools.tsx
@@ -4,15 +4,39 @@ import { useState } from "react";
 import { School } from "@/lib/schema";
 import { IconPlus } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 
 export default function AdminSchools({ schools }: any) {
     let [search, setSearch] = useState("");
     let sch: School[] = schools.filter((v: School) => v.name.indexOf(search) >= 0);
     let [opened, { open, close }] = useDisclosure(false);
 
+    let form = useForm({
+        initialValues: {
+            name: "",
+            domain: "",
+            owner: "",
+            address: "",
+            primary_color: "blue",
+            logo: ""
+        },
+        validate: {
+            name: (v) => v.length > 0 ? null : "Please do not leave this empty",
+            domain: (v) => v.length > 0 ? !v.startsWith("@") ? null : "Please remove '@'" : "Please do not leave this empty",
+            owner: (v) => v.length > 0 ? null : "Please do not leave this empty",
+            address: (v) => v.length > 0 ? null : "Please do not leave this empty",
+        }
+    });
+
     const onSearch = (e: any) => {
         let str = e.currentTarget.value;
         setSearch(str);
+    }
+
+    const createSchool = (sd: any) => {
+        let s: School = {
+            ...sd
+        }
     }
 
     return (
@@ -35,7 +59,9 @@ export default function AdminSchools({ schools }: any) {
             </Container>
 
             <Modal opened={opened} onClose={close} title="Create a New School" centered>
+                <form onSubmit={form.onSubmit(createSchool)}>
 
+                </form>
             </Modal>
         </>
     );
