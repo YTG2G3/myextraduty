05aae42af98e2b99ed1a3dd378f1c15cc91bd05a
YTG2G3 <scratchlover05@gmail.com>
2023-04-23 17:11:57 -0700

    user complete

--- components/Admin/AdminUsers.tsx
+++ components/Admin/AdminUsers.tsx
@@ -1,6 +1,8 @@
 import { User } from '@/lib/schema';
 import styles from '@/styles/AdminUsers.module.scss';
-import { Accordion, TextInput } from '@mantine/core';
+import { Accordion, ActionIcon, Group, Stack, Text, TextInput, Tooltip } from '@mantine/core';
+import { IconUserShield, IconUserX } from '@tabler/icons-react';
+import Image from 'next/image';
 import { useState } from "react";
 
 export default function AdminUsers({ users }: any) {
@@ -19,15 +21,35 @@ export default function AdminUsers({ users }: any) {
     }
 
     // TODO - pagination and effcient searching without loading the whole table
+    // TODO - manage users' enrollments
     return (
         <div className={styles.container}>
             <TextInput style={{ width: "100%" }} placeholder="Search" value={search} onChange={onSearch} />
 
-            <Accordion>
-                {us.map((v, i) => (
+            <Accordion style={{ width: "100%", marginTop: 20 }}>
+                {new Array(30).fill(us[0]).map((v, i) => (
                     <Accordion.Item key={i} value={v.email}>
                         <Accordion.Control>{v.name}</Accordion.Control>
-                        <Accordion.Panel>{v.email}</Accordion.Panel>
+
+                        <Accordion.Panel>
+                            <Group>
+                                <Image src={v.picture} width={100} height={100} alt={v.name} />
+
+                                <Stack ml="lg">
+                                    <Text>Email: {v.email}</Text>
+
+                                    <Group style={{ width: "100%", justifyContent: "center" }}>
+                                        <Tooltip label="Assign Admin Role">
+                                            <ActionIcon><IconUserShield color="#339AF0" /></ActionIcon>
+                                        </Tooltip>
+
+                                        <Tooltip label="Reset">
+                                            <ActionIcon><IconUserX color='red' /></ActionIcon>
+                                        </Tooltip>
+                                    </Group>
+                                </Stack>
+                            </Group>
+                        </Accordion.Panel>
                     </Accordion.Item>
                 ))}
             </Accordion>
--- lib/theme.ts
+++ lib/theme.ts
@@ -2,6 +2,7 @@ import { MantineThemeOverride } from '@mantine/core';
 import { Ubuntu } from 'next/font/google';
 const ubuntu = Ubuntu({ weight: '400', subsets: ['latin'] });
 
+// TODO - darkmode
 export default function theme(primaryColor: string) {
     let t: MantineThemeOverride = {
         colorScheme: "light",
