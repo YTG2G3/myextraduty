5b4851366742a674efc0a4e9c69b354da46a1754
YTG2G3 <scratchlover05@gmail.com>
2023-05-13 20:59:38 -0700

    added dates and forced install

--- components/Manager/ManagerSettings.tsx
+++ components/Manager/ManagerSettings.tsx
@@ -1,6 +1,7 @@
 import SiteContext from '@/lib/site-context';
 import styles from '@/styles/ManagerSettings.module.scss';
-import { Button, MANTINE_COLORS, Select, TextInput } from '@mantine/core';
+import { Button, Group, MANTINE_COLORS, Select, TextInput } from '@mantine/core';
+import { modals } from '@mantine/modals';
 import { notifications } from '@mantine/notifications';
 import { useContext, useState } from 'react';
 
@@ -32,6 +33,34 @@ export default function ManagerSettings() {
         setLoading(false);
     }
 
+    const transferReq = async (e: any) => {
+        e.preventDefault();
+
+        let b = { email: e.target.owner.value };
+
+        let s = (await fetch("/api/school/transfer", { method: "POST", body: JSON.stringify(b), headers: { school: String(school.id) } })).status;
+
+        if (s === 200) {
+            modals.closeAll();
+            notifications.show({ title: "Success!", message: "Please refresh after about 10 seconds for the system to update." });
+        }
+        else notifications.show({ title: "Failed to trasfer ownership", message: "Please confirm that the owner's MyExtraDuty account has been created.", color: "red" });
+    }
+
+    const transferOwner = () => modals.open({
+        title: `Transfer ownership of ${school.name}`,
+        centered: true,
+        children: (
+            <form onSubmit={(e) => transferReq(e)}>
+                <TextInput name="owner" withAsterisk label="New Owner Email" />
+
+                <Group position="right" mt="md">
+                    <Button type="submit" color='red'>Transfer</Button>
+                </Group>
+            </form>
+        )
+    });
+
     // TODO - opening at and quota etc
     return (
         <form className={styles.container} onSubmit={saveChanges}>
@@ -40,7 +69,7 @@ export default function ManagerSettings() {
             <Select name="primary_color" withAsterisk label="School Color" data={MANTINE_COLORS.map((v) => ({ value: v, label: v }))} defaultValue="blue" />
 
             <div className={styles.ho}>
-                <Button loading={loading} m="lg" color="red">Transfer Ownership</Button>
+                <Button loading={loading} m="lg" color="red" onClick={transferOwner}>Transfer Ownership</Button>
                 <Button loading={loading} m="lg" type="submit">Save</Button>
             </div>
         </form>
