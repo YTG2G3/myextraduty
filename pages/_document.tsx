8d31084d4b304b7e95e57120190532c0c886000f
YTG2G3 <scratchlover05@gmail.com>
2023-05-18 23:48:02 -0700

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,9 @@ export default class _Document extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <link rel="shortcut icon" href="/images/favicon.ico" />
+                </Head>
 
                 <body>
                     <Main />
