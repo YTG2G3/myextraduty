cda1f7069ef34d823b49f348934cf79232a514ac
YTG2G3 <scratchlover05@gmail.com>
2023-07-16 01:05:08 -0400

    dashboard overlay changed

--- components/Manager/ManagerDashboard.tsx
+++ components/Manager/ManagerDashboard.tsx
@@ -2,14 +2,13 @@ import SiteContext from "@/lib/site-context";
 import dayjs from "dayjs";
 import { useContext } from "react";
 import GetReady from "../GetReady";
+import { Text } from "@mantine/core";
 
 // TODO - upcoming events, quota, status of members
 export default function ManagerDashboard(props: any) {
     let { school } = useContext(SiteContext);
 
-    if (!school.opening_at) return (
-        <div>To start the countdown, please set the opening date in settings.</div>
-    );
+    if (!school.opening_at) return <Text>To start the countdown, please set the opening date in settings.</Text>
 
     if (dayjs(school.opening_at).isAfter(dayjs())) return <GetReady />
 
