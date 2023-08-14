780107f72d0919d760715034dfad414dc6ba96b2
YTG2G3 <scratchlover05@gmail.com>
2023-08-14 00:42:34 -0700

    minor bug fix

--- components/Manager/ManagerTasks.tsx
+++ components/Manager/ManagerTasks.tsx
@@ -191,7 +191,7 @@ export default function ManagerTasks({ tasks, categories, assignments, members }
                         <ActionIcon variant="filled" mr="xs" onClick={uploadTasks}><IconUpload /></ActionIcon>
                     </Tooltip>
 
-                    <CSVLink style={{ marginRight: 10 }} data={[["Category", "Name", "Description", "Starting Date", "Starting Time", "Ending Date", "Ending Time", "Capacity"], ...tasks.map(v => [v.category, v.name, v.description, v.starting_date, v.ending_date, v.starting_time, v.ending_time, v.capacity])]} filename={`${school.name} tasks (${new Date().getUTCFullYear()})`}>
+                    <CSVLink style={{ marginRight: 10 }} data={[["Category", "Name", "Description", "Starting Date", "Ending Date", "Starting Time", "Ending Time", "Capacity"], ...tasks.map(v => [v.category, v.name, v.description, v.starting_date, v.ending_date, v.starting_time, v.ending_time, v.capacity])]} filename={`${school.name} tasks (${new Date().getUTCFullYear()})`}>
                         <Tooltip label="Download">
                             <ActionIcon variant="filled"><IconDownload /></ActionIcon>
                         </Tooltip>
