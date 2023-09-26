6dbe318baade00bbecd5eab18173db2a786420b8
YTG2G3 <scratchlover05@gmail.com>
2023-09-25 18:20:29 -0700

    extra tests

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -9,5 +9,9 @@ export default function handler(req: any, res: any) {
     res.json({
         server: dayjs(),
         serverInTZ: dayjs().tz('America/Los_Angeles'),
+        serverInKorea: dayjs().tz('Asia/Seoul'),
+        serverStr: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+        serverStrInTZ: dayjs().tz('America/Los_Angeles').format('YYYY-MM-DD HH:mm:ss'),
+        serverStrInKorea: dayjs().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss'),
     });
 }
