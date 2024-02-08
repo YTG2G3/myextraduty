3c0897491deacf37682f0c1110613d4a9ce7c877
YTG2G3 <scratchlover05@gmail.com>
2024-02-07 23:53:14 -0800

    prisma change

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,10 +1,9 @@
-import { bricolage } from "@/app/fonts";
 import { Separator } from "../ui/separator";
 
 export default function Hero({ title, description }: { title: string, description: string }) {
     return (
         <div className="w-full flex justify-center items-center flex-col mt-36">
-            <p className={`font-extrabold text-5xl mb-3 text-center ${bricolage.className}`}>{title}</p>
+            <p className={`font-extrabold text-5xl mb-3 text-center font-grotesque`}>{title}</p>
 
             <p className="text-2xl">{description}</p>
 
