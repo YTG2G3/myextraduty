71c17a77dc6a43767ba47cfcc55087320fac9bb4
YTG2G3 <scratchlover05@gmail.com>
2024-02-07 23:57:15 -0800

    prisma globalThis

--- lib/db.ts
+++ lib/db.ts
@@ -8,4 +8,5 @@ const prisma = globalThis.prisma || new PrismaClient();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+globalThis.prisma = prisma;
