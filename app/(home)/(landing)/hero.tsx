e20862502bcab61e86c78a06ce60acb8fab98195
issac4892 <issac4892@gmail.com>
2024-02-08 10:11:21 +0900

    fix margin & use bricolage in title

--- app/(home)/(landing)/hero.tsx
+++ app/(home)/(landing)/hero.tsx
@@ -1,10 +1,11 @@
+import { bricolage } from "@/app/fonts";
 import { ChevronsDown, MousePointer } from "lucide-react";
 
 export default function Hero() {
     return (
         <div className="w-full h-screen flex justify-center items-center flex-col relative">
             <div className="flex items-center sm:flex-col md:flex-col lg:flex-row ">
-                <p className="font-extrabold text-7xl mb-3 text-center mr-3">As simple as clicking a button</p>
+                <p className={`font-extrabold text-7xl mb-3 text-center mr-3 ${bricolage.className}`}>As simple as clicking a button</p>
                 <MousePointer width={60} height={60} />
             </div>
 
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,9 +1,10 @@
+import { bricolage } from "@/app/fonts";
 import { Separator } from "../ui/separator";
 
 export default function Hero({ title, description }: { title: string, description: string }) {
     return (
         <div className="w-full flex justify-center items-center flex-col mt-36">
-            <p className="font-extrabold text-5xl mb-3 text-center mr-3">{title}</p>
+            <p className={`font-extrabold text-5xl mb-3 text-center ${bricolage.className}`}>{title}</p>
 
             <p className="text-2xl">{description}</p>
 
