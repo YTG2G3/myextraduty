4b7c86c79215a969872d517390d39ff3a84637ee
issac4892 <issac4892@gmail.com>
2024-02-11 18:10:30 +0900

    not found & move nav

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,17 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { bricolage } from "./fonts";
+import { useRouter } from 'next/navigation';
 
 export default function FourOFour() {
+  const router = useRouter();
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="flex flex-col items-center gap-4">
         <span className={`${bricolage.className} text-9xl opacity-40 font-black`}>404</span>
         <span>The requested page could not be found.</span>
-        <Button>Go back to the home page</Button>
+        <Button onClick={() => router.back()}>Go back</Button>
       </div>
     </div>
   );
