67188c8574700d712c333964c2147c9fb904de06
Mingeon Kim <issac4892@gmail.com>
2024-02-12 20:53:43 +0900

    minor bugfix

--- app/(auth)/school/new/page.tsx
+++ app/(auth)/school/new/page.tsx
@@ -6,13 +6,22 @@ import { useEffect } from 'react';
 export default function NewInit() {
   useEffect(() => {
     let basic = sessionStorage.getItem('basic');
-    if (!basic) navigate('/school/new/basic');
+    if (!basic) {
+      navigate('/school/new/basic');
+      return;
+    }
 
     let plan = sessionStorage.getItem('plan');
-    if (!plan) navigate('/school/new/plan');
+    if (!plan) {
+      navigate('/school/new/plan');
+      return;
+    }
 
     let advanced = sessionStorage.getItem('advanced');
-    if (!advanced) navigate('/school/new/advanced');
+    if (!advanced) {
+      navigate('/school/new/advanced');
+      return;
+    }
 
     navigate('/school/new/complete');
   }, []);
