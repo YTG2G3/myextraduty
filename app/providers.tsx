6cf029f84b9cb34720ca60d380ecdd99aa126bf0
issac4892 <issac4892@gmail.com>
2024-02-13 15:05:02 +0900

    fix

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,19 @@
 
 import { Toaster } from '@/components/ui/sonner';
 import { AppProgressBar } from 'next-nprogress-bar';
+import { Suspense } from 'react';
 
 export default function Providers({ children }) {
   return (
     <>
       {children}
-      <AppProgressBar
-        height="2px"
-        color="#0096FF"
-        options={{ showSpinner: false }}
-      />
+      <Suspense fallback={null}>
+        <AppProgressBar
+          height="2px"
+          color="#0096FF"
+          options={{ showSpinner: false }}
+        />
+      </Suspense>
       <Toaster richColors />
     </>
   );
