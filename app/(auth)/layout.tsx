cd05309fb46c792afb02a5973409175f055daff3
issac4892 <issac4892@gmail.com>
2024-02-17 23:08:45 +0900

    suspense

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,6 +4,7 @@ import getServerSession from '@/lib/get-server-session';
 import AuthProvider from './auth-provider';
 import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
+import { toast } from 'sonner';
 
 // TODO - alert feature as sonner
 export default async function AuthLayout({
@@ -12,7 +13,10 @@ export default async function AuthLayout({
   children: React.ReactNode;
 }>) {
   let session = await getServerSession();
-  if (!session) redirect('/auth/signin');
+  if (!session) {
+    toast.error('You have to sign in first.')
+    redirect('/auth/signin');
+  }
 
   // Check if there are any invitations
   async function loadData() {
