a0580561e3353015dffcc05ef144daf6e7cde1c0
YTG2G3 <scratchlover05@gmail.com>
2024-02-19 05:23:35 -0500

    invitation

--- app/(console)/layout.tsx
+++ app/(console)/layout.tsx
@@ -1,70 +1,10 @@
-import prisma from '@/lib/db';
 import InvitationDialog from '@/components/utils/invitation-dialog';
-import getServerSession from '@/lib/get-server-session';
-import { Suspense } from 'react';
-import { redirect } from 'next/navigation';
-import { toast } from 'sonner';
 
-// TODO - alert feature as sonner
 export default async function AuthLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Check if there are any invitations
-  // async function loadData() {
-  //   'use server';
-
-  //   // TODO - they have to be ordered properly. the commented code below is sample code to do it.
-  //   // let data = await prisma.invitation.findMany({
-  //   //   where: { email: session.user.email }
-  //   // }).then(invitations => {
-  //   //   return Promise.all(invitations.map(invitation =>
-  //   //     prisma.school.findUnique({
-  //   //       where: { id: invitation.schoolId }
-  //   //     }).then(school =>
-  //   //       prisma.user.findUnique({
-  //   //         where: { id: school.ownerId }
-  //   //       }).then(owner => ({ invitation, school, owner }))
-  //   //     )
-  //   //   ));
-  //   // });
-  //   let invitations = await prisma.invitation.findMany({
-  //     where: { email: session.user.email }
-  //   });
-  //   let schools = await prisma.school.findMany({
-  //     where: {
-  //       id: { in: invitations.map((invitation) => invitation.schoolId) }
-  //     }
-  //   });
-  //   let owners = await prisma.user.findMany({
-  //     where: { id: { in: schools.map((school) => school.ownerId) } }
-  //   });
-
-  //   return { invitations, schools, owners };
-  // }
-
-  // Receive invitation id and school id
-  // async function decide(
-  //   id: string,
-  //   schoolId: string,
-  //   manager: boolean,
-  //   accept: boolean
-  // ) {
-  //   'use server';
-
-  //   if (accept)
-  //     prisma.enrollment.create({
-  //       data: {
-  //         userId: session.user.id,
-  //         schoolId,
-  //         manager
-  //       }
-  //     });
-
-  //   prisma.invitation.delete({ where: { id } });
-  // }
-
   return (
     <>
       <InvitationDialog />
