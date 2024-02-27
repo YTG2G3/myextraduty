6da5a9635ee6052fc69a2adbd5b84b03778c5beb
YTG2G3 <scratchlover05@gmail.com>
2024-02-26 20:13:51 -0800

    header wrapping for alert

--- app/(console)/school/[id]/alert/page.tsx
+++ app/(console)/school/[id]/alert/page.tsx
@@ -1,6 +1,11 @@
+import HeaderWrapper from '../header-wrapper';
 import RoleRoute from '../role-route';
 import User from './user';
 
 export default async function Alert({ params }: { params: { id: string } }) {
-  return <RoleRoute id={params.id} user={User} manager={null} />;
+  return (
+    <HeaderWrapper title="Alerts">
+      <RoleRoute id={params.id} user={User} manager={null} />
+    </HeaderWrapper>
+  );
 }
